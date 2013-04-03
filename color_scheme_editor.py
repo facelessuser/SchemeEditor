d6c71eb337805f18dd99d6664265d267298cec5d
Isaac <muse_i@MuseI-Opt990.wdl.wdc.com>
2013-04-03 14:45:17 -0600

    -Add new features to plugin side.

--- color_scheme_editor.py
+++ color_scheme_editor.py
@@ -3,6 +3,12 @@ import sublime_plugin
 from os.path import join, exists, basename, normpath, dirname
 from os import makedirs
 import subprocess
+import re
+from .default_new_theme import theme as default_new_theme
+import json
+import codecs
+from plistlib import writePlistToBytes
+
 
 PLUGIN_NAME = "ColorSchemeEditor"
 THEME_EDITOR = None
@@ -11,6 +17,40 @@ TEMP_PATH = "Packages/User/%s" % TEMP_FOLDER
 PLUGIN_SETTINGS = 'color_scheme_editor.sublime-settings'
 PREFERENCES = 'Preferences.sublime-settings'
 SCHEME = "color_scheme"
+LATEST_VERSION = "0.0.2"
+
+MSGS = {
+    "version": '''Color Scheme Editor:
+    You are currently running version %s of
+    subclrschm, %s is the expected version.
+    Some features may not work. Please consider
+    updating the editor for the best possible
+    experience.
+
+    Do you want to ignore this update?
+    ''',
+
+    "linux": '''Color Scheme Editor:
+    Sorry, currently no love for the penguin.
+    Linux support coming in the future.
+    ''',
+
+    "access": '''Color Scheme Editor:
+    subclrschm cannot be accessed.
+    ''',
+
+    "binary": '''Color Scheme Editor:
+    Could not find subclrschm (the editor)!
+    ''',
+
+    "temp": '''Color Scheme Editor:
+    Could not copy theme file to temp directory.
+    ''',
+
+    "new": '''Color Scheme Editor:
+    Could not create new theme.
+    '''
+}
 
 
 class ColorSchemeEditorLogCommand(sublime_plugin.WindowCommand):
@@ -21,17 +61,30 @@ class ColorSchemeEditorLogCommand(sublime_plugin.WindowCommand):
 
 
 class ColorSchemeEditorCommand(sublime_plugin.ApplicationCommand):
-    def run(self, no_direct_edit=True, current_theme=False):
+    def run(self, no_direct_edit=True, current_theme=False, new_theme=False):
         if THEME_EDITOR is None:
             return
         # Get current color scheme
-        p_settigns = sublime.load_settings(PLUGIN_SETTINGS)
+        p_settings = sublime.load_settings(PLUGIN_SETTINGS)
         settings = sublime.load_settings(PREFERENCES)
         scheme_file = settings.get(SCHEME, None)
         actual_scheme_file = None
+        open_json = False
 
+        if new_theme:
+            actual_scheme_file = join(sublime.packages_path(), "User", TEMP_FOLDER, "new_theme.tmTheme")
+            scheme_file = "%s/%s" % (TEMP_PATH, "new_theme.tmTheme")
+            try:
+                with codecs.open(actual_scheme_file, "w", "utf-8") as f:
+                    f.write(writePlistToBytes(default_new_theme).decode('utf-8'))
+            except Exception as e:
+                sublime.error_message(MSGS["new"])
+                return
 
-        if scheme_file is not None and current_theme:
+            # Load new theme
+            settings.set(SCHEME, scheme_file)
+
+        elif scheme_file is not None and current_theme:
             # Get real path
             actual_scheme_file = join(dirname(sublime.packages_path()), normpath(scheme_file))
 
@@ -52,29 +105,65 @@ class ColorSchemeEditorCommand(sublime_plugin.ApplicationCommand):
                     with open(actual_scheme_file, "wb") as f:
                         f.write(text)
                 except:
-                    sublime.error_message("Color Scheme Editor:\nCould not copy theme file to temp directory.")
+                    sublime.error_message(MSGS["temp"])
                     return
 
                 # Load unarchived theme
-                scheme_file = settings.set(SCHEME, "%s/%s" % (TEMP_PATH, basename(scheme_file)))
+                settings.set(SCHEME, "%s/%s" % (TEMP_PATH, basename(scheme_file)))
 
         # Call the editor with the theme file
         subprocess.Popen(
             [THEME_EDITOR] +
-            (["-d"] if bool(p_settigns.get("debug", False)) else []) +
+            (["-d"] if bool(p_settings.get("debug", False)) else []) +
+            (["-j"] if open_json else []) +
             ["-l", join(sublime.packages_path(), "User")] +
             ([actual_scheme_file] if actual_scheme_file is not None and exists(actual_scheme_file) else [])
         )
 
 
+def check_version(editor, p_settings):
+    p = subprocess.Popen([editor, "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = p.communicate()
+    m = re.match(r"subclrschm ([\d]*\.[\d]*\.[\d])*", out[1].decode('utf-8'))
+    if m is not None:
+        version = m.group(1)
+        if version != LATEST_VERSION:
+            ignore_versions = p_settings.get("ignore_version_update", [])
+            if not LATEST_VERSION in ignore_versions:
+                if sublime.ok_cancel_dialog(MSGS["version"] % (version, LATEST_VERSION), "Ignore"):
+                    ignore_versions.append(LATEST_VERSION)
+                    p_settings.set("ignore_version_update", ignore_versions)
+                    sublime.save_settings(PLUGIN_SETTINGS)
+    else:
+        sublime.error_message(MSGS["access"])
+
+
+def parse_binary_path(pth):
+    return normpath(pth).replace("${Packages}", sublime.packages_path())
+
+
 def plugin_loaded():
     global THEME_EDITOR
     platform = sublime.platform()
+    p_settings = sublime.load_settings(PLUGIN_SETTINGS)
+    p_settings.clear_on_change('reload')
 
-    # Pick the correct binary for the editor
-    if platform == "osx":
-        THEME_EDITOR = join(sublime.packages_path(), PLUGIN_NAME, "subclrschm.app", "Contents", "MacOS", "subclrschm")
-    elif platform == "windows":
-        THEME_EDITOR = join(sublime.packages_path(), PLUGIN_NAME, "subclrschm.exe")
-    elif platform == "linux":
-        sublime.error_message("Color Scheme Editor:\nSorry, currently no love for the penguin.\nLinux support coming in the future.")
+    try:
+        # Pick the correct binary for the editor
+        if platform == "osx":
+            THEME_EDITOR = join(parse_binary_path(p_settings.get("osx")), "Contents", "MacOS", "subclrschm")
+        elif platform == "windows":
+            THEME_EDITOR = parse_binary_path(p_settings.get("windows"))
+        elif platform == "linux":
+            sublime.error_message(MSGS["linux"])
+    except:
+        pass
+
+    if THEME_EDITOR is None or not exists(THEME_EDITOR):
+        sublime.error_message(MSGS["binary"])
+        THEME_EDITOR = None
+
+    elif THEME_EDITOR is not None:
+        check_version(THEME_EDITOR, p_settings)
+
+    p_settings.add_on_change('reload', plugin_loaded)
---
+++ default_new_theme.py
@@ -0,0 +1,75 @@
+theme = {
+    "name": "Grayscale",
+    "settings": [
+        {
+            "settings": {
+                "activeGuide": "#888888",
+                "background": "#FFFFFF",
+                "bracketContentsForeground": "#000000",
+                "bracketContentsOptions": "underline",
+                "bracketsForeground": "#000000",
+                "bracketsOptions": "underline",
+                "caret": "#000000",
+                "findHighlight": "#666666",
+                "findHighlightForeground": "#000000",
+                "foreground": "#000000",
+                "gutter": "#E5E5E5",
+                "gutterForeground": "#000000",
+                "inactiveSelection": "#888888",
+                "invisibles": "#323232",
+                "lineHighlight": "#2E2E2E22",
+                "selection": "#666666",
+                "selectionForeground": "#FFFFFF",
+                "tagsOptions": "stippled_underline"
+            }
+        },
+        {
+            "name": "Comment",
+            "scope": "comment",
+            "settings": {
+                "foreground": "#A5A5A5"
+            }
+        },
+        {
+            "name": "Storage type",
+            "scope": "storage.type",
+            "settings": {
+                "fontStyle": "italic",
+                "foreground": "#000000"
+            }
+        },
+        {
+            "name": "Class name",
+            "scope": "entity.name.class",
+            "settings": {
+                "fontStyle": "underline",
+                "foreground": "#000000"
+            }
+        },
+        {
+            "name": "Inherited class",
+            "scope": "entity.other.inherited-class",
+            "settings": {
+                "fontStyle": "italic underline",
+                "foreground": "#000000"
+            }
+        },
+        {
+            "name": "Function argument",
+            "scope": "variable.parameter",
+            "settings": {
+                "fontStyle": "italic",
+                "foreground": "#000000"
+            }
+        },
+        {
+            "name": "Library class/type",
+            "scope": "support.type, support.class",
+            "settings": {
+                "fontStyle": "italic",
+                "foreground": "#000000"
+            }
+        }
+    ],
+    "uuid": "07379361-ce49-4e6c-a03f-26378a7a2131"
+}
